f1936c27043f1f2b3bf192e4b2886ca963786f5e
caitlin <caitlin.wink@gmail.com>
2021-03-26 09:45:19 -0700

    commit tree 8c3054eb2d971de4dd2e5bcf3afaf6ff44bc061e

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat (Ralph){
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(Broom){
+    const catCopy = [...cats , 'Broom'];
+    return catCopy;
+}
+function prependCat(Arnold){
+    const catCopy2 = ['Arnold', ...cats];
+    return catCopy2;
+}
+function removeLastCat(){
+    const catsSlice = cats.slice(0,2);
+    return catsSlice;
+}
+function removeFirstCat(){
+    const catsSplice = cats.slice(1);
+    return catsSplice;
+}
+
